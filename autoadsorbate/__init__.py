d33490b8d73e0925a9fb3450e92b4c26558ffa8d
tiagojoaog <tiagojoaog@gmail.com>
2026-03-16 15:46:45 +0000

    updated ipynb doc, updated package version

--- autoadsorbate/__init__.py
+++ autoadsorbate/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Fakoe Edvin"""
 __email__ = "edvinfako@gmail.com"
-__version__ = "0.2.6"
+__version__ = "0.2.7"
 
 from autoadsorbate.autoadsorbate import Fragment, Surface
 from autoadsorbate.Smile import get_marked_smiles
